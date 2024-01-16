c32d477afa9eddaefe4d247cc9bf721b066a5717
ClaudiaCavagna <claudia.cavagna96@gmail.com>
2024-01-16 22:06:30 +0100

    small style changes

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -8,31 +8,13 @@ import { FaHeart } from "react-icons/fa";
 
 
 const Card = styled('div')`
-  height: 120px;
-  width: 100px;
+  height: 100px;
+  width: 80px;
   border-radius: 4px 4px 0px 0px;
   overflow-x: hidden;
   position: relative;
   transform: translateZ(0);
   margin: 2px;
-  /* margin-bottom: 3rem; */
-  @media screen and (min-width: 430px){
-    width: 140px;
-    height: 160px;
-  }
-  @media screen and (min-width: 630px){
-    width: 170px;
-    height: 180px;
-  }
-  @media screen and (min-width: 768px){
-    width: 220px;
-    height: 245px;
-  }
-  @media screen and (min-width: 992px) {
-    width: 350px;
-    height: 330px;
-    margin: 4px;
-  }
   &:hover {
     .card-action {
       opacity: 1;
@@ -44,13 +26,53 @@ const Card = styled('div')`
   .card-action {
     opacity: 0;
     visibility: hidden;
-    transition: all 125ms linear;
+    transition: all 125ms ease-out;
     transform: rotateX(-90deg);
     width: 100%;
     overflow: hidden;
     position: absolute;
     bottom: 0px;
     background-color: var(--translucent);
+    padding: 0.5rem;
+  }
+  @media screen and (min-width: 375px){
+    width: 100px;
+    height: 120px;
+    .card-action {
+      padding: 1rem;
+    }
+  }
+  @media screen and (min-width: 490px){
+    width: 140px;
+    height: 160px;
+    .card-action {
+      padding: 1.5rem;
+    }
+  }
+  @media screen and (min-width: 630px){
+    width: 170px;
+    height: 180px;
+    .card-action{
+      padding: 2rem;
+    }
+  }
+  @media screen and (min-width: 768px){
+    width: 220px;
+    height: 245px;
+    .card-action{
+      padding: 3rem;
+    }
+  }
+  @media screen and (min-width: 992px) {
+    width: 300px;
+    height: 280px;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 330px;
+    height: 350px;
+    .card-action {
+      padding: 4rem;
+    }
   }
 `;
 
@@ -66,10 +88,13 @@ const Photo = ({alt_description, color, urls: {regular}, likes, id}) => {
         <div style={{width: '100%', height: '100%', background: {color}}}>
           <img src={regular} alt={alt_description} onLoad={()=>setLoad(true)} style={{width: '100%', height: '100%'}} />   
           <div className='card-action' style={{display: 'grid', alignItems: 'center'}}>
-              <p>{likes} 
+            <div className='d-flex justify-content-between'>
+              <p className=''>{likes} 
                 <FaHeart style={{color: 'var(--purple-300)', marginInline: '5px', marginBottom: '3px'}}/>
               </p>
               <p>{price}€</p>
+            </div>
+              
               <button className='btn'>
                 <CartIcon className='cart-icon-small' />
               </button>
@@ -83,4 +108,4 @@ const Photo = ({alt_description, color, urls: {regular}, likes, id}) => {
   )
 }
 
-export default Photo
+export default Photo;
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,8 +10,7 @@ export const rowalizer = (array, items_per_row = 3) => {
 
 export const pricer = (likes) => {
     let price = '0';
-    likes % 2 ? price='10' : price='5';
-
+    likes % 2 ? price='20' : price='15';
     return price;
-}
+};
 
